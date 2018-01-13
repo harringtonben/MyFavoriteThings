1c8729d839176d4d3ae0e909b5d5bbdc98ad5c72
Ben Harrington <harringtonben@icloud.com>
2018-01-13 14:28:17 -0600

    working through favorite things

---
+++ MyFavoriteThings/MyFavoriteThings/Things/Beers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MyFavoriteThings.Things
+{
+    class Beers
+    {
+       
+        private string _ipas;
+        private string _stouts;
+
+        public string ipas
+        {
+            get
+            {
+                return _ipas;
+            }
+            set
+            {
+                _ipas = value;
+            }
+        }
+
+        public string stouts
+        {
+            get
+            {
+                return _stouts;
+            }
+            set
+            {
+                _stouts = value;
+            }
+        }
+
+        public bool Drinking(string drinking)
+        {
+            drinking = _stouts;
+            return true;
+        }
+
+        public string ImOut()
+        {
+            return ("I'll have another");
+        }
+    }
+}
