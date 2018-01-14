67ab2e5b8f35f04b3038c4c96a2bb54dcf65e6f9
Ben Harrington <harringtonben@icloud.com>
2018-01-14 15:26:45 -0600

    working through final thing

---
+++ MyFavoriteThings/MyFavoriteThings/Things/Steaks.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+namespace MyFavoriteThings.Things
+{
+    class Steaks
+    {
+        private string _type;
+        private string _cookingtemp;
+        private bool _iscookedwell;
+
+        public string Type
+        {
+            get
+            {
+                return _type;
+            }
+            set
+            {
+                _type = value;
+            }
+        }
+
+        public string CookingTemp
+        {
+            get
+            {
+                return _cookingtemp;
+            }
+            set
+            {
+                _cookingtemp = value;
+            }
+        }
+
+        public bool IsCookedWell
+        {
+            get
+            {
+                return true;
+            }
+            set
+            {
+                if (value)
+                {
+                    _iscookedwell = true;
+                }
+                if (!value)
+                {
+                    _iscookedwell = false;
+                }
+            }
+        }
+
+
+    }
+}
