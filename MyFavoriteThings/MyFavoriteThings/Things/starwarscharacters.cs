f1e565c8e2d873b78e24873adf3868a04cc528b2
Ben Harrington <harringtonben@icloud.com>
2018-01-14 14:55:39 -0600

    working through second favorite thing

---
+++ MyFavoriteThings/MyFavoriteThings/Things/starwarscharacters.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyFavoriteThings.Things
+{
+    class starwarscharacters
+    {
+        private string _jedi;
+        private string _sith;
+        private bool _isdead;
+
+        public string Jedi
+        {
+            get
+            {
+                return _jedi;
+            }
+            set
+            {
+                _jedi = value;
+            }
+        }
+
+        public string Sith
+        {
+            get
+            {
+                return _sith;
+            }
+            set
+            {
+                _sith = value;
+            }
+        }
+    }
+}
