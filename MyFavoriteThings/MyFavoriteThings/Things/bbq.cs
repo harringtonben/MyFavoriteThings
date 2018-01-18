2c388b814ef681dc3072ad3028b411e4bc5298bf
Ben Harrington <harringtonben@icloud.com>
2018-01-18 17:34:46 -0600

    finished last favorite thing class

---
+++ MyFavoriteThings/MyFavoriteThings/Things/bbq.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFavoriteThings.Things
+{
+    class bbq
+    {
+        public string Cut { get; set; }
+        public string Style { get; set; }
+        public bool IsTender { get; set; }
+
+        public string BBQReview()
+        {
+            return IsTender
+                ? $"I had some really good {Style} {Cut} today and it was incredibly tender!"
+                : $"I had some awful {Style} {Cut} today...hated every second of it.";
+        }
+    }
+}
